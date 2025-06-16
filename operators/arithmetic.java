26c60f8d9bf61af435ac4ee0161392c86af6c2ce
chakrareethika-04 <reethikadasari2004@gmail.com>
2025-06-16 16:10:20 +0530

    Add files via upload

---
+++ operators/arithmetic.java
@@ -0,0 +1,15 @@
+ import java.io.*;
+public class Arithmetic
+{
+public static void main(String[] args)
+{
+int a=3;
+int b=6;
+System.out.println("add="+(a+b));
+System.out.println("sub="+(a-b));
+System.out.println("mul="+(a*b));
+System.out.println("div="+(a/b));
+System.out.println("modu="+(a%b));
+}
+}
+
