4d47dec3cbac7fbea25960444d341faee4780356
chakrareethika-04 <reethikadasari2004@gmail.com>
2025-06-17 14:41:56 +0530

    Add files via upload

---
+++ defaultvalues/var.java
@@ -0,0 +1,25 @@
+//Default values
+import java.io.*;
+import java.util.Scanner;
+public class var
+{
+static byte b;
+static short s;
+static int i;
+static long l;
+static float f;
+static double d;
+static char c;
+static boolean bo;
+public static void main(String[] args)
+{
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(c);
+System.out.println(bo);
+}
+}
